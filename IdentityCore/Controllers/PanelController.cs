452a4f7b01b81e39d8dbd699ce3a13038893b29a
JobTracking <tuncalin@outlook.com>
2021-12-24 01:39:22 +0300

    User Update- Resim, İmage Update işlemler

--- IdentityCore/Controllers/PanelController.cs
+++ IdentityCore/Controllers/PanelController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -20,19 +21,54 @@ namespace IdentityCore.Controllers
         }
         public async Task<IActionResult> Index()
         {
-           var user= await _userManager.FindByNameAsync(User.Identity.Name);
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
             return View(user);
         }
         public async Task<IActionResult> UpdateUser()
         {
             var user = await _userManager.FindByNameAsync(User.Identity.Name);
-            UserUpdateViewModel model = new UserUpdateViewModel {
-             Email=user.Email,
-              Name=user.Name,
-               PhoneNumber=user.PhoneNumber,
-                PictureUrl=user.PictureUrl,
-                 SurName=user.SurName
+            UserUpdateViewModel model = new UserUpdateViewModel
+            {
+                Email = user.Email,
+                Name = user.Name,
+                PhoneNumber = user.PhoneNumber,
+                PictureUrl = user.PictureUrl,
+                SurName = user.SurName
             };
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateUser(UserUpdateViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByNameAsync(User.Identity.Name);
+                //Resim Upload
+                if (model.Picture != null)
+                {
+                    var uygulamanınCalistigiYer = Directory.GetCurrentDirectory();
+                    var uzanti = Path.GetExtension(model.Picture.FileName);
+                    var resimAd = Guid.NewGuid() + uzanti;
+                    var kaydedilecekYer = uygulamanınCalistigiYer + "/wwwroot/img/" + resimAd; 
+                    //Stream nesnesi maliyetlidir. O yüzden İşlem bitince GC temizlemesi için using kullanılır. 
+                    using var stream = new FileStream(kaydedilecekYer, FileMode.Create);
+                    await model.Picture.CopyToAsync(stream);
+                    user.PictureUrl = resimAd;
+                }
+                user.Name = model.Name;
+                user.SurName = model.SurName;
+                user.Email = model.Email;
+                user.PhoneNumber = model.PhoneNumber; 
+                var result= await  _userManager.UpdateAsync(user);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description); 
+                }
+            }
             return View(model);
         }
         [AllowAnonymous]
