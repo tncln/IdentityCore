90a540079264b34ce1fe5d1c084e6d93a0de698c
JobTracking <tuncalin@outlook.com>
2021-12-25 23:13:06 +0300

    Rol işlemleri / Listeleme

---
+++ IdentityCore/Controllers/RolController.cs
@@ -0,0 +1,25 @@
+﻿using IdentityCore.Context;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityCore.Controllers
+{
+    [Authorize]
+    public class RolController : Controller
+    {
+        private readonly RoleManager<AppRole> _roleManager;
+        public RolController(RoleManager<AppRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+        public IActionResult Index()
+        {
+            return View(_roleManager.Roles.ToList());
+        }
+    }
+}
